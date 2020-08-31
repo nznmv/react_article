e91d5fcb18abfb0eaa3711ab63a5f52d9f7fea48
nznmvs <nznmvs@gmail.com>
2020-08-31 17:52:59 +0300

    Updates

---
+++ precache-manifest.7a7e604da6582b7065428cfeed931170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ff5d39e4bea82a085a4d669bd0b42f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ebb41580dd51017fc25",
+    "url": "./static/js/2.dd291650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dd291650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034f595aaf31cc37e4b3",
+    "url": "./static/js/main.f159dfea.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.7a7e604da6582b7065428cfeed931170.js"
 );
 
 self.addEventListener('message', (event) => {
